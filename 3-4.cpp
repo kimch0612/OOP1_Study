95acfe1f065fbbfd92cd06bc143e2b5ebd435b23
Changhwan Kim <kimch061279@gmail.com>
2023-03-20 07:25:06 +0000

    a

---
+++ 3-4.cpp
@@ -0,0 +1,11 @@
+// 예제 3-12
+#include <iostream>
+using namespace std;
+int main(){
+    int value;
+    cout << "값을 입력하세요!! : ";
+    cin >> value;
+    cout << ((value > 0) ? " 양수입니다!!" :
+    (value = 0) ? " 0입니다!!" : " 음수입니다!! ");
+    cout << endl;
+}
