060a2734d22a995b51153daae4178541acd87c13
Changhwan Kim <kimch061279@gmail.com>
2023-03-20 06:17:21 +0000

    asd'

---
+++ 3-1.cpp
@@ -0,0 +1,6 @@
+// 예제 2-1
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Char : " << sizeof(char) << "byte.\n";
+}
---
+++ 3-2.cpp
@@ -0,0 +1,9 @@
+// 예제 2-14
+#include <iostream>
+using namespace std;
+int main(){
+    bool a;
+    a = 100;
+    cout << a << endl;
+    cout << sizeof(a) << endl;
+}
